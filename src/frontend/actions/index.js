f74393340ba99fd2b38ec069a7c81dd9114d0da1
Niko Santis <nikosantis@gmail.com>
2019-10-30 18:04:18 -0300

    first commit

---
+++ src/frontend/actions/index.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+export const setFavorite = payload => ({
+  type: 'SET_FAVORITE',
+  payload,
+});
+
+export const deleteFavorite = payload => ({
+  type: 'DELETE_FAVORITE',
+  payload,
+});
+
+export const loginRequest = payload => ({
+  type: 'LOGIN_REQUEST',
+  payload,
+});
+
+export const logoutRequest = payload => ({
+  type: 'LOGOUT_REQUEST',
+  payload,
+});
+
+export const setError = payload => ({
+  type: 'SET_ERROR',
+  payload,
+});
+
+export const registerRequest = payload => ({
+  type: 'REGISTER_REQUEST',
+  payload,
+});
+
+export const getVideoSource = payload => ({
+  type: 'GET_VIDEO_SOURCE',
+  payload,
+});
+
+export const registerUser = (payload, redirectUrl) => {
+  return (disptach) => {
+    axios.post('/auth/sign-up', payload)
+      .then(({ data }) => disptach(registerRequest(data)))
+      .then(() => {
+        window.location.href = redirectUrl;
+      })
+      .catch(err => disptach(setError(err)));
+  };
+};
---
+++ src/server/routes/main.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { StaticRouter } from 'react-router';
+import { renderRoutes } from 'react-router-config';
+import axios from 'axios';
+import Routes from '../../frontend/routes/serverRoutes';
+import Layout from '../../frontend/components/Layout';
+import reducer from '../../frontend/reducers';
+import render from '../render';
+
+require('dotenv').config();
+
+const main = async (req, res, next) => {
+  try {
+    let initialState;
+    try {
+      const { token, email, name, id } = req.cookies;
+      let user = {};
+      if (email || name || id) {
+        user = {
+          id,
+          email,
+          name,
+        };
+      }
+
+      let movieList = await axios({
+        url: `${process.env.API_URL}/api/movies`,
+        headers: { Authorization: `Bearer ${token}` },
+        method: 'get',
+      });
+
+      let userMovies = await axios({
+        url: `${process.env.API_URL}/api/movies?userId=${id}`,
+        headers: { Authorization: `Bearer ${token}` },
+        method: 'get',
+      });
+
+      movieList = movieList.data.data;
+      userMovies = userMovies.data.data;
+
+      initialState = {
+        user,
+        playing: {},
+        myList: userMovies.filter(movie => movie._id === id),
+        trends: movieList.filter(movie => movie.contentRating === 'PG' && movie.id),
+        originals: movieList.filter(movie => movie.contentRating === 'G' && movie.id),
+      };
+
+    } catch (err) {
+      initialState = {
+        user: {},
+        playing: {},
+        myList: [],
+        trends: {},
+        originals: {},
+      };
+    }
+    const store = createStore(reducer, initialState);
+    const html = renderToString(
+      <Provider store={store}>
+        <StaticRouter
+          location={req.url}
+          context={{}}
+        >
+          <Layout>
+            {renderRoutes(Routes)}
+          </Layout>
+        </StaticRouter>
+      </Provider>,
+    );
+    const preloadedState = store.getState();
+    res.send(render(html, preloadedState));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default main;
